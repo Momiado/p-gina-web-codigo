e69016b435e0a4311dcf2edb862170e84e323358
Momiado <32756623+Momiado@users.noreply.github.com>
2017-11-10 09:53:48 -0600

    Add files via upload

---
+++ sitio proyrecto/js/script.js
@@ -0,0 +1,8 @@
+;
+(function (d, w, n, c, $) {
+  //Programación que se activa cuando el documento HTML ha cargado en el navegador
+  $(d).ready(function () {
+    //método que activa el la barra de navegación
+    $(".button-collapse").sideNav()
+  })
+})(document, window, navigator, console.log, jQuery)
